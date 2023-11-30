52d5c901b3b9627795772c0d721dfe322f2fc19a
刘为 <m13153689201@163.com>
2023-11-30 10:35:30 +0800

    更新文件 Gui.py

--- Gui.py
+++ Gui.py
@@ -1,25 +1,30 @@
 import sys
 from Crawl import *
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton
-from PyQt5.QtCore import QThread, pyqtSignal, QTimer, Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QCheckBox
+from PyQt5.QtCore import QThread, pyqtSignal
+from ScriptsCrawl import *
+from NovelCrawl import *
 
 class SpiderThread(QThread):
     def __del__(self):
-        self.spider.save_state()
+        for spider in self.spiders:
+            spider.save_state()
     log_signal = pyqtSignal(str)
 
-    def __init__(self, spider):
+    def __init__(self, spiders):
         super().__init__()
-        self.spider = spider
-
+        self.spiders = spiders
+        
     def run(self):
         try:
-            self.spider.run()
+            for spider in self.spiders:
+                spider.run()
         except Exception as e:
             self.log_signal.emit(f"Error: {str(e)}")
         finally:
             # 执行清理操作，确保关闭时保存状态
-            self.spider.save_state()
+            for spider in self.spiders:
+                spider.save_state()
 
 class LogReaderThread(QThread):
     log_signal = pyqtSignal(str)
@@ -43,32 +48,45 @@ class LogReaderThread(QThread):
 class MyGUI(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.checkboxes = []
         self.initUI()
 
     def initUI(self):
-        self.log_text = QTextEdit(self)
-        self.start_button = QPushButton('Start Spider', self)
+        self.start_button = QPushButton('开始爬取', self)
         self.start_button.clicked.connect(self.startSpider)
+        self.log_text = QTextEdit(self)
+        self.checkbox_layout = QVBoxLayout()
 
         vbox = QVBoxLayout()
-        vbox.addWidget(self.log_text)
         vbox.addWidget(self.start_button)
-
+        vbox.addLayout(self.checkbox_layout)
+        vbox.addWidget(self.log_text)
         self.setLayout(vbox)
 
         self.setGeometry(300, 300, 400, 300)
         self.setWindowTitle('Spider GUI')
         self.show()
+        # 在 __initUI 方法中的 self.checkbox_layout 后添加以下代码
+        for spider_class in [SfyCrawler,ScifiscriptsCrawler,AwesomefilmCrawler,moviescriptsandscreenplaysCrawler,XinanCrawler,TibetNovelCrawl,ChinawriterCrawler]:  # 添加更多的爬虫类
+            checkbox = QCheckBox(spider_class.__name__, self)
+            self.checkboxes.append(checkbox)
+            self.checkbox_layout.addWidget(checkbox)
 
     def startSpider(self):
-        spider = SfyCrawler()  # 创建你的爬虫类实例
-        self.spider_thread = SpiderThread(spider)
+        selected_spiders = [spider_class for spider_class, checkbox in zip([SfyCrawler, ScifiscriptsCrawler, AwesomefilmCrawler, moviescriptsandscreenplaysCrawler, XinanCrawler, TibetNovelCrawl, ChinawriterCrawler], self.checkboxes) if checkbox.isChecked()]
+
+        if not selected_spiders:
+            self.log_text.append("No spider selected.\n")
+            return
+
+        spiders = [spider_class(state_file=f"{spider_class.__name__}_state.json") for spider_class in selected_spiders]
+
+        self.spider_thread = SpiderThread(spiders)
         self.spider_thread.log_signal.connect(self.updateLog)
         self.spider_thread.start()
 
-        log_file = f"{spider.__class__.__name__}.log"
-        self.log_reader_thread = LogReaderThread(log_file)
+        log_files = [f"{spider_class.__name__}.log" for spider_class in selected_spiders]
+        self.log_reader_thread = LogReaderThread(log_files[0])  # Assuming only one log file for simplicity
         self.log_reader_thread.log_signal.connect(self.updateLog)
         self.log_reader_thread.start()
 
@@ -77,7 +95,9 @@ class MyGUI(QWidget):
         self.log_text.append(log)
 
     def closeEvent(self, event):
-        self.spider_thread.spider.save_state()
+        if hasattr(self, 'spider_thread'):
+            for spider in self.spider_thread.spiders:
+                spider.save_state()
         event.accept()
 
 if __name__ == '__main__':
